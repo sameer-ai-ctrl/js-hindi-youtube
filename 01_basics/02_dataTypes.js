a976f0cea44db6989dbc6338d037a2f3571af477
sameer-ai-ctrl <sameersahaik123456@gmail.com>
2026-09-06 04:15:18 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3) // we are usind node js, not browser
+
+console.log(3 
+    + 
+    3) //code readability should be high "tc39.es"
+
+console.log("sameer")    
+
+let name = "sameer"
+let age = 22
+let isApproved = true
+let state;
+
+// number => 2 to the power 53
+// bigint
+//string => ""
+// boolean => true/false
+//null => empty value
+//undefined => value is not assigned
+//symbol => unique identifier
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
