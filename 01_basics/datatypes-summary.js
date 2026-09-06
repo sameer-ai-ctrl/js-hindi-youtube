e8fcc0aaa33f7f6f14ba73ca0f81afefbe55629f
sameer-ai-ctrl <sameersahaik123456@gmail.com>
2026-09-06 11:40:55 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,34 @@
+// primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+// console.log(id === anotherId);
+
+// const bigNumber = 1234567890123456789012345678901234567890n;
+
+
+// Reference (Non primitive) : Object, Array, Function
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj ={
+    name: "sameer",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log
+}
+
+console.log(typeof heros);
+
